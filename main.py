01235bb05c8e5cb1d48099636a3654426efa1c65
Zaid <solankizaid63@gmail.com>
2026-07-12 22:36:33 +0530

    Add Task Feature added

--- main.py
+++ main.py
@@ -3,23 +3,28 @@
 # Store all tasks
 tasks = []
 
-# Take input from the user
-title = input("Enter Task Title: ")
-description = input("Enter Task Description: ")
-priority = input("Enter Task Priority: ")
-due_date = input("Enter Task Due Date: ")
+choice = "yes"
 
-# Create a task (Dictionary)
-task = {
+
+while (choice =="yes"):
+    # Take input from the user
+    title = input("Enter Task Title: ")
+    description = input("Enter Task Description: ")
+    priority = input("Enter Task Priority: ")
+    due_date = input("Enter Task Due Date: ")
+    # Create a task (Dictionary)
+    task = {
     "title": title,
     "description": description,
     "priority": priority,
     "due_date": due_date,
     "completed": False
 }
+    # Add the task to the tasks list
+    tasks.append(task)
+    choice = input("Do you want to add another task? (yes/no): ")
 
-# Add the task to the tasks list
-tasks.append(task)
+
 
 # Display all tasks
 print("\nTask List")
